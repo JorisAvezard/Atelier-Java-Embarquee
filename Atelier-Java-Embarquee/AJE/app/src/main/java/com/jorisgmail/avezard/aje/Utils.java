24e14bf9d5dd95067787dd92f1bdb0b0a53a609b
Mallaurie PHCAR <phcarmallaurie2@gmail.com>
2017-10-25 11:27:46 +0200

    Update Utils.java

--- Atelier-Java-Embarquee/AJE/app/src/main/java/com/jorisgmail/avezard/aje/Utils.java
+++ Atelier-Java-Embarquee/AJE/app/src/main/java/com/jorisgmail/avezard/aje/Utils.java
@@ -6,7 +6,7 @@ package com.jorisgmail.avezard.aje;
 public class Utils {
 
     //String Values to be Used in App
-    public static final String downloadDirectory = "AJE03";
+    public static final String downloadDirectory = "AJE Downloaded Videos";
     public static final String mainUrl = "https://www.quirksmode.org/html5/videos/";
     public static final String downloadVideoUrl = "https://www.quirksmode.org/html5/videos/big_buck_bunny.mp4";
 }
